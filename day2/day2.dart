cb9a7d5131326d9a5cf107bc2952cccc1e408704
xsonority <philippklingert123@gmail.com>
2023-12-19 22:59:32 +0100

    readFIle

--- day2/day2.dart
+++ day2/day2.dart
@@ -1,3 +1,11 @@
+import 'dart:io';
 main() {
+  readFile();
   
+}
+
+List<dynamic> readFile() {
+  final inputdayone = File("input.txt");
+  List<dynamic> lines = inputdayone.readAsLinesSync();
+  return lines;
 }
